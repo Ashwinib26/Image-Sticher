8242918236812a0c3a369dcd343c80432a7bc9ac
Ashwini Bisen <ashwinibisen26@gmail.com>
2025-04-08 18:53:36 +0530

    File Updates

--- app.py
+++ app.py
@@ -5,15 +5,12 @@ import os
 
 app = Flask(__name__)
 
-# Set upload folder path
 UPLOAD_FOLDER = os.path.join('static', 'uploads')
 RESULT_FOLDER = os.path.join('static', 'results')
 
-# Create directories if they don't exist
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(RESULT_FOLDER, exist_ok=True)
 
-# Add to Flask config
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['RESULT_FOLDER'] = RESULT_FOLDER
 
@@ -46,16 +43,13 @@ def stitch_images(path1, path2):
     gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-    # Feature detection
     sift = cv2.SIFT_create()
     kp1, des1 = sift.detectAndCompute(gray1, None)
     kp2, des2 = sift.detectAndCompute(gray2, None)
 
-    # Match features
     flann = cv2.FlannBasedMatcher(dict(algorithm=1, trees=5), dict(checks=50))
     matches = flann.knnMatch(des1, des2, k=2)
 
-    # Ratio test
     good = [m for m, n in matches if m.distance < 0.75 * n.distance]
 
     if len(good) < 4:
@@ -66,11 +60,9 @@ def stitch_images(path1, path2):
 
     H, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
 
-    # Warp image1 to image2's perspective
     height1, width1 = img1.shape[:2]
     height2, width2 = img2.shape[:2]
 
-    # Get size of final panorama
     corners_img1 = np.float32([[0, 0], [0, height1], [width1, height1], [width1, 0]]).reshape(-1, 1, 2)
     warped_corners = cv2.perspectiveTransform(corners_img1, H)
     all_corners = np.concatenate((warped_corners, np.float32([[0, 0], [0, height2], [width2, height2], [width2, 0]]).reshape(-1, 1, 2)), axis=0)
@@ -81,10 +73,8 @@ def stitch_images(path1, path2):
     translation = [-xmin, -ymin]
     H_translate = np.array([[1, 0, translation[0]], [0, 1, translation[1]], [0, 0, 1]])
 
-    # Warp image1
     stitched_img = cv2.warpPerspective(img1, H_translate.dot(H), (xmax - xmin, ymax - ymin))
-    
-    # Paste image2 onto canvas
+   
     stitched_img[translation[1]:height2 + translation[1], translation[0]:width2 + translation[0]] = img2
 
     result_path = os.path.join(RESULT_FOLDER, "stitched_result.jpg")
